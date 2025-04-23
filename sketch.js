428e7f180771c2239d4cb9fe3a9bd12206741256
GreenSlider21 <greenslider21@gmail.com>
2025-04-22 19:51:19 -0600

    added an attack and made digging
    you can no longer move diagonally and will dig a tunnel when in dirt sections. However touching the edge breaks the game. There is also the rough outline for the start of an attack but it doesn't look at what direction your facing yet.

--- sketch.js
+++ sketch.js
@@ -8,17 +8,22 @@
 // constants
 const EMPTY = 0;
 const DIGABLE = 1;
-const PLAYER = 9;
-const POOKA = 8;
-const FYGAR = 7;
 const ROCK = 2;
+const PUMP = 3;
+const FYGAR = 7;
+const POOKA = 8;
+const PLAYER = 9;
 const CELL_SIZE = 20;
 const ROWS = 32;
 const COLS = 28;
+const WALKDELAY = 200;
+const DIGDELAY = 400;
 
 // varriables
 let grid;
 let level;
+let walkTime = 0;
+let digTime = 0;
 
 // classes
 class Character {
@@ -27,6 +32,8 @@ class Character {
     this.y = y;
     this.colour = "blue";
     this.speed = 1;
+    this.pumpColour = "purple"
+    this.attcking = false
   }
 
   display() {
@@ -39,35 +46,65 @@ class Character {
   
   move() {
     console.log(this.x, this.y);
+    // alows the player to move the four cardinal directions, but disallows diagnal movment
+    if (this.attcking === false){
       if (keyIsDown(87) === true) {
         // move up
         this.y -= this.speed;
       }
-      if (keyIsDown(83) === true) {
+      else if (keyIsDown(83) === true) {
         // move down
         this.y += this.speed;
       }
-      if (keyIsDown(65) === true) {
+      else if (keyIsDown(65) === true) {
         // move left
         this.x -= this.speed;
       }
-      if (keyIsDown(68) === true) {
+      else if (keyIsDown(68) === true) {
         // move right
         this.x += this.speed;
       }
+    }
   }
 
-  tunnelMove() {
-    if (this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && grid[this.y][this.x] === EMPTY || 
-      this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && grid[this.y][this.x] === PLAYER) {
+  attck() {
+    if (keyIsDown(32) === true) {
+      this.attcking = true;
+      fill(this.pumpColour);
+      square(this.x * CELL_SIZE + 2 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
+      square(this.x * CELL_SIZE + 3 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
+      square(this.x * CELL_SIZE + 4 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
+    }
+    else {
+      this.attcking = false;
+    }
+  }
+
+  playerMove() {
+   // movement that is specifically triggered and slower by digging new tunnels
+   if (this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && 
+    (grid[this.y][this.x] === DIGABLE || grid[this.y+1][this.x] === DIGABLE ||grid[this.y][this.x+1] === DIGABLE ||grid[this.y+1][this.x+1] === DIGABLE)) {
+      // slower digging delay
+      if (millis() - digTime > DIGDELAY) {
+        digTime = millis();
+        // deletes old tiles to make tunnels
+        grid[this.y][this.x] = EMPTY;
+        grid[this.y+1][this.x] = EMPTY;
+        grid[this.y][this.x+1] = EMPTY;
+        grid[this.y+1][this.x+1] = EMPTY;
+        
         this.move();
       }
-  }
-
-  digMove() {
-    if (this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && 
-      (grid[this.y][this.x] === DIGABLE || grid[this.y+1][this.x] === DIGABLE ||grid[this.y][this.x+1] === DIGABLE ||grid[this.y+1][this.x+1] === DIGABLE)) {
-        this.move();
+    }
+    
+    // movement that is specifically triggered by walking in tunnels
+    else if (this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && grid[this.y][this.x] === EMPTY || 
+      this.x >= 1 && this.x < COLS - 2 && this.y >= 1 && this.y < ROWS - 2 && grid[this.y][this.x] === PLAYER) {
+        // faster tunnel delay
+        if (millis() - walkTime > WALKDELAY) {
+          walkTime = millis();
+          this.move();
+        }
       }
   }
 }
@@ -88,8 +125,8 @@ function draw() {
   background(220);
   displayGrid();
 
-  taizo.tunnelMove();
-  taizo.digMove();
+  taizo.attck();
+  taizo.playerMove();
 
   taizo.display();
 }
@@ -105,12 +142,11 @@ function displayGrid() {
         fill("black");
         square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
-      else if (grid[y][x] === PLAYER) {
-        fill("blue");
-        square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
-        square(x * CELL_SIZE + 1, y * CELL_SIZE, CELL_SIZE);
-        square(x * CELL_SIZE, y * CELL_SIZE + 1, CELL_SIZE);
-        square(x * CELL_SIZE + 1, y * CELL_SIZE + 1, CELL_SIZE);
+      else if (grid[y][x] === PUMP) {
+        fill("purple");
+        square(x * CELL_SIZE + 2, y * CELL_SIZE, CELL_SIZE);
+        square(x * CELL_SIZE + 3, y * CELL_SIZE, CELL_SIZE);
+        square(x * CELL_SIZE + 4, y * CELL_SIZE, CELL_SIZE);
       }
       else if (grid[y][x] === POOKA) {
         fill("orange");
