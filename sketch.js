bdfd8c9f92864d78328280dc5945c5612e8fe3cc
GreenSlider21 <greenslider21@gmail.com>
2025-06-09 14:16:42 -0600

    made some final tweaks and added some needed comments

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 // DIG DUG Clone
 // Liam Prange
-// April - Ongoing
+// April - June
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Got an actually working sound system that detect a multitude of diffent sounds and what to play when
 
 // constants
 const EMPTY = 0;
@@ -12,7 +12,7 @@ const CELL_SIZE = 20;
 const ROWS = 32;
 const COLS = 28;
 const WALKDELAY = 150;
-const DIGDELAY = 200;
+const DIGDELAY = 300;
 const MUSICDELAY = 181000;
 const PUMPDELAY = 2000;
 
@@ -56,6 +56,7 @@ class Character {
     this.hurtDelay = 200;
   }
   
+  // shows the player
   display() {
     fill(this.colour);
     square(this.x * CELL_SIZE, this.y * CELL_SIZE, CELL_SIZE);
@@ -64,6 +65,7 @@ class Character {
     square(this.x * CELL_SIZE + 1 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
   }
 
+  // ads an extra life if the player reaches 1000, 2000, 3000, ect. points
   heal() {
     if (score >= scoreLives) {
       scoreLives += 1000;
@@ -71,6 +73,7 @@ class Character {
     }
   }
 
+  // removes a life if the player is hit and resets their position and the enemies positions
   hurt() {
     if (playerHit === true) {
       if (millis() - hurtTime > this.hurtDelay) {
@@ -89,6 +92,7 @@ class Character {
     }
   }
 
+  // triggers the game over state when the player has lost all their lives
   gameOver() {
     if (this.lives > 0) {
       gameState = "play";
@@ -99,23 +103,28 @@ class Character {
     }
   }
   
+  // controls the players movement
   move() {
     let nextX = this.x;
     let nextY = this.y;
   
     if (this.attcking === false){
+      // move up with w or up arrow
       if (keyIsDown(87) || keyIsDown(38)) {
         nextY -= this.speed;
         this.facingDirection = "up";
       }
+      // move down with s or down arrow
       else if (keyIsDown(83) || keyIsDown(40)) {
         nextY += this.speed;
         this.facingDirection = "down";
       }
+      // move left with a or left arrow
       else if (keyIsDown(65) || keyIsDown(37)) {
         nextX -= this.speed;
         this.facingDirection = "left";
       }
+      // move right with d or right arrow
       else if (keyIsDown(68) || keyIsDown(39)) {
         nextX += this.speed;
         this.facingDirection = "right";
@@ -155,25 +164,30 @@ class Character {
     }
   }
   
+  // checks the direction the player last moved in and attacks in that direction
   attck() {
     if (keyIsDown(32) === true) {
       this.attcking = true;
       fill(this.pumpColour);
+      // attack up
       if (this.facingDirection === "up"){
         square(this.x * CELL_SIZE, this.y * CELL_SIZE - 1 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE, this.y * CELL_SIZE - 2 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE, this.y * CELL_SIZE - 3 * CELL_SIZE, CELL_SIZE);
       }
+      // attack down
       else if (this.facingDirection === "down"){
         square(this.x * CELL_SIZE, this.y * CELL_SIZE + 2 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE, this.y * CELL_SIZE + 3 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE, this.y * CELL_SIZE + 4 * CELL_SIZE, CELL_SIZE);
       }
+      // attack left
       else if (this.facingDirection === "left"){
         square(this.x * CELL_SIZE - 1 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE - 2 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE - 3 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
       }
+      // attack right
       else if (this.facingDirection === "right"){
         square(this.x * CELL_SIZE + 2 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
         square(this.x * CELL_SIZE + 3 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
@@ -206,7 +220,9 @@ class Enemy {
     this.attack = true;
   }
 
+  // shows the enemies 
   display() {
+    // change to green if not at full health otherwise stays orange
     if (this.health < 3) {
       this.colour = "green";
     }
@@ -220,6 +236,7 @@ class Enemy {
     square(this.x * CELL_SIZE + 1 * CELL_SIZE, this.y * CELL_SIZE + 1 * CELL_SIZE, CELL_SIZE);
   }
 
+  // if they lose all their health splice them out of theEnemies array and add them to the deadEnemies array
   life() {
     if (this.health <= 0) {
       kills++;
@@ -234,6 +251,7 @@ class Enemy {
     }
   }
 
+  // stop them from damaging the player if they arn't at full health
   attacking() {
     if (this.health < 3) {
       this.attack = false;
@@ -267,6 +285,7 @@ class Enemy {
     // detecing if the enemy is hit by the player attack
     if (millis() - this.hurtTime > this.hurtDelay) {
       this.hurtTime = millis();
+      // detects if the enemy needs takes damage only if the players attack is touching the enemy
       if (keyIsDown(32) === true) {
         if (taizo.facingDirection === "up" &&
           ((this.x === this.end.playerX || this.x + 1 === this.end.playerX) && 
@@ -302,6 +321,7 @@ class Enemy {
         }
       }
       else {
+        // heals the enemy health after a delay
         if (this.health < 3 && millis() - this.healTime > this.healDelay) {
           this.healTime = millis();
           this.health ++;
@@ -310,11 +330,14 @@ class Enemy {
     }
   }
 
+  // controls the enemy movement
   move() {
+    // only move while at full health
     if (this.hit === false && this.health === 3){
       if (millis() - this.enemyTime > this.delay) {
         this.enemyTime = millis();
   
+        // if still in a wall keep ghosting
         if (grid[this.y][this.x] !== EMPTY || grid[this.y + 1][this.x] !== EMPTY 
             || grid[this.y][this.x + 1] !== EMPTY || grid[this.y + 1][this.x + 1] !== EMPTY) {
           this.ghosting = true;
@@ -323,7 +346,8 @@ class Enemy {
         else {
           this.ghosting = false;
         }
-  
+        
+        // on a five percent chance allow moving through walls
         let gamble;
         
         if (this.ghosting === false){
@@ -431,6 +455,7 @@ function draw() {
   gameStateCheck();
 }
 
+// pressing p puases and resumes the game
 function keyTyped() {
   if (gameState === "pause" && key === "p") {
     gameState = "play";
@@ -440,8 +465,9 @@ function keyTyped() {
   }
 }
 
+// check if the game is paused, playing, or finished to allow or dissallow movement and trigger sounds and text
 function gameStateCheck() {
-    if (gameState === "pause") {
+  if (gameState === "pause") {
     textSize(50);
     textAlign(CENTER);
     textFont("Impact");
@@ -477,6 +503,7 @@ function gameStateCheck() {
   }
 }
 
+// plays a song or sound if the condition is met
 function jukebox() {
   // music
   if (musicState === "play") {
@@ -522,17 +549,20 @@ function jukebox() {
   }
 }
 
+// creates four enemies
 function enemySpawner() {
   for (let i = 0; i < 4; i++) {
     spawnEnemy(xSpawns[i], ySpawns[i]);
   }
 }
 
+// puts the enemies in theEnemies array so that they are in play
 function spawnEnemy(x, y) {
   let someEnemy = new Enemy(x, y, taizo.x, taizo.y);
   theEnemies.push(someEnemy);
 }
 
+// checks if all the enemies are dead, if so calls new ones into play
 function needMoreEnemies () {
   if (theEnemies.length < 4 && deadEnemies.length % 4 === 0) {
     enemySpawner();
@@ -543,6 +573,7 @@ function needMoreEnemies () {
   }
 }
 
+// resets player position if it's a new level
 function resetLevel() {
   if (level > prevoiusLevel) {
     prevoiusLevel++;
@@ -551,6 +582,7 @@ function resetLevel() {
   }
 }
 
+// displays the grid and sets each tile to empty or digable apropriatly based on the grid
 function displayGrid() {
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
@@ -566,6 +598,7 @@ function displayGrid() {
   }
 }
 
+// displays the player score and highscore with lables
 function displayScore() {
   textSize(30);
   textAlign(CENTER);
@@ -578,6 +611,7 @@ function displayScore() {
   text(highScore, width-50, 190);
 }
 
+// displays the players lives and how many points they need for an extra life
 function displayLives() {
   textSize(30);
   textAlign(CENTER);
@@ -594,6 +628,7 @@ function displayLives() {
   text(":", 90, height-9);
 }
 
+// displays how many kills the player has and shows all the dead enemies
 function displayKills() {
   textSize(30);
   textAlign(CENTER);
@@ -614,6 +649,7 @@ function displayKills() {
   }
 }
 
+// displays the goal and controls while making them white when pressed
 function displayControls() {
   textSize(15);
   textAlign(CENTER);
@@ -654,19 +690,19 @@ function displayControls() {
   }
   text("D", width-40, 315);
 
-    if (keyIsDown(32)) {
+  if (keyIsDown(32)) {
     fill("white");
   }
   else {
     fill("dodgerblue");
   }
-  text("SPACE", width-60, 370);
+  text("HOLD SPACE", width-60, 370);
 
   fill("blue");
   text("YOU", width-80, 450);
   square(width-90, 470, CELL_SIZE);
 
-  fill("orange")
+  fill("orange");
   text("THEM", width-40, 450);
   square(width-50, 470, CELL_SIZE);
 }
